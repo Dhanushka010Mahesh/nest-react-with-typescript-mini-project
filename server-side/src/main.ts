282aac6f70187728b230be16f1d2e3743779e30f
Dhanushka010Mahesh <E2240438@bit.uom.lk>
2025-10-15 10:10:11 +0530

    mini project setup

---
+++ server-side/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api/v1');
+  await app.listen(process.env.PORT ?? 6050);
+  console.log(`Server is running on port http://localhost:${process.env.PORT ?? 6050}`);
+}
+bootstrap();
